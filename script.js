e54f424d41b2439710f4c217af2f76d0006005fa
AnacondaPython <acpython777@gmail.com>
2017-06-23 16:57:14 -0400

    Added support for Smooth transition Hours+minutes

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+
 
 var date = new Date();
 console.log(date);
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
+
+let hrPosition = 360*hr/12 + (360*(min/60)/12); //hours are 1-24 values
+let minPosition = (360*min/60) +(360*(sec/60)/60); //minutes are 1-59 values
+let secPosition = 360*sec/60 //seconds are 1-59 values
 
 console.log("Hours: ", hr, "Minutes: ", min, "Seconds: ", sec);
 console.log("Hours: " + hr, "Minutes: " + min, "Seconds: " + sec);
