33417703dd760a8f6952e656b1a6e7b418a1f299
AnacondaPython <acpython777@gmail.com>
2017-06-23 17:09:47 -0400

    http://i.imgur.com/OV959ES.gif Bandaid fix to reset 59sec

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 
+function runTheClock() {
+  var date = new Date();
+  console.log(date);
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
 
-var date = new Date();
-console.log(date);
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
+  let hrPosition = 360*hr/12 + (360*(min/60)/12); //hours are 1-24 values
+  let minPosition = (360*min/60) +(360*(sec/60)/60); //minutes are 1-59 values
+  let secPosition = 360*sec/60 //seconds are 1-59 values
 
-let hrPosition = 360*hr/12 + (360*(min/60)/12); //hours are 1-24 values
-let minPosition = (360*min/60) +(360*(sec/60)/60); //minutes are 1-59 values
-let secPosition = 360*sec/60 //seconds are 1-59 values
-
-console.log("Hours: ", hr, "Minutes: ", min, "Seconds: ", sec);
-console.log("Hours: " + hr, "Minutes: " + min, "Seconds: " + sec);
+  console.log("Hours: ", hr, "Minutes: ", min, "Seconds: ", sec);
+  console.log("Hours: " + hr, "Minutes: " + min, "Seconds: " + sec);
 
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock,1000); //runs clock 1000ms=1s
