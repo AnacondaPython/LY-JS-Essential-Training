767e1e856ec7e25e62cfd432591d8b3b4ab24d3b
ACpython <acpython777@gmail.com>
2017-07-28 17:26:46 -0400

    create object containing diff size values (viewport, responsive images)

--- 10_02-ResponsiveImg/JS/script.js
+++ 10_02-ResponsiveImg/JS/script.js
@@ -1,5 +1,11 @@
 const IMAGES = document.querySelectorAll("img");
-
+const SIZES = {
+    showcase: "100vw",
+    reason: "(max-width: 799px) 100 vw, 372px",
+    feature: "(max-width: 799px) 100 vw, 558px",
+    story: "(max-width: 799px) 100 vw, 670px",
+};
+//Generate new responsive image name strings
 function makeSrcset(imgSrc){
   let markup =[];
   let width = 400;
@@ -12,6 +18,7 @@ function makeSrcset(imgSrc){
   return markup.join();
 }
 
+//Loops each unique image, call func, create 5x images (400,800,1200,1600,2000)
 for (let i = 0; i < IMAGES.length; i++){
   let imgSrc = IMAGES[i].getAttribute("src");
   imgSrc = imgSrc.slice(0,-8);
@@ -19,5 +26,6 @@ for (let i = 0; i < IMAGES.length; i++){
   console.log(srcseta);
 
   let type = IMAGES[i].getAttribute("data-type");
+  let sizes = SIZES[type]; //SIZES.type no work b.c searches for prop name
   console.log(type);
 }
