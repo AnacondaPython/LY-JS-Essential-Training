00a639b357388455f89669a8d2b52144547f9a6b
AnacondaPython <acpython777@gmail.com>
2017-07-30 23:16:15 -0400

    http://i.imgur.com/hmvqgwP.png Upd img markup with srcset and size. .jpg

--- 10_02-ResponsiveImg/JS/script.js
+++ 10_02-ResponsiveImg/JS/script.js
@@ -1,9 +1,9 @@
 const IMAGES = document.querySelectorAll("img");
 const SIZES = {
     showcase: "100vw",
-    reason: "(max-width: 799px) 100 vw, 372px",
-    feature: "(max-width: 799px) 100 vw, 558px",
-    story: "(max-width: 799px) 100 vw, 670px",
+    reason: "(max-width: 799px) 100vw, 372px",
+    feature: "(max-width: 799px) 100vw, 558px",
+    story: "(max-width: 799px) 100vw, 670px",
 };
 //Generate new responsive image name strings
 function makeSrcset(imgSrc){
@@ -11,7 +11,7 @@ function makeSrcset(imgSrc){
   let width = 400;
 
   for (let i = 0; i<5; i++){
-    markup[i] = imgSrc + "-" + width + "w";
+    markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
     width+=400;
   }
 
@@ -23,9 +23,11 @@ for (let i = 0; i < IMAGES.length; i++){
   let imgSrc = IMAGES[i].getAttribute("src");
   imgSrc = imgSrc.slice(0,-8);
   let srcseta = makeSrcset(imgSrc);
+  IMAGES[i].setAttribute("srcset",srcseta);
   console.log(srcseta);
 
   let type = IMAGES[i].getAttribute("data-type");
   let sizes = SIZES[type]; //SIZES.type no work b.c searches for prop name
+  IMAGES[i].setAttribute("sizes", sizes);
   console.log(type);
 }
